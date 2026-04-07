8491ed6c33170a0c655fe68a12446065c1d70c7b
momsi <momsi@callisto>
2026-04-07 09:40:30 +0200

    disko correction

--- hosts/gaia/disko.nix
+++ hosts/gaia/disko.nix
@@ -13,9 +13,9 @@
               label = "boot";
               size = "513MiB";
               type = "EF00";
-              format = {
+              content = {
+                type = "filesystem";
                 format = "vfat";
-                label = "boot";
               };
             };
             root = {
@@ -27,38 +27,37 @@
                 settings = {
                   allowDiscards = true;
                 };
-                format = {
-                  format = "btrfs";
-                  label = "luks-root";
-                  extraMountOptions = "compress=zstd,noatime";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
                   subvolumes = {
                     "@" = {
                       mountpoint = "/";
-                      mountOptions = "compress=zstd,noatime";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                     "@home" = {
                       mountpoint = "/home";
-                      mountOptions = "compress=zstd,noatime";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                     "@log" = {
                       mountpoint = "/var/log";
-                      mountOptions = "compress=zstd,noatime";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                     "@cache" = {
                       mountpoint = "/var/cache";
-                      mountOptions = "compress=zstd,noatime";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                     "@tmp" = {
                       mountpoint = "/var/tmp";
-                      mountOptions = "compress=zstd,noatime";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                     "@root" = {
                       mountpoint = "/root";
-                      mountOptions = "compress=zstd,noatime";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                     "@srv" = {
                       mountpoint = "/srv";
-                      mountOptions = "compress=zstd,noatime";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                   };
                 };
@@ -82,14 +81,13 @@
                 settings = {
                   allowDiscards = true;
                 };
-                format = {
-                  format = "btrfs";
-                  label = "luks-data";
-                  extraMountOptions = "compress=zstd,noatime";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
                   subvolumes = {
                     "/nosys" = {
                       mountpoint = "/nosys";
-                      mountOptions = "compress=zstd,noatime";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                   };
                 };
