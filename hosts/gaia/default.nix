91ca2e4b5c0a9589929cdca058158249f2355215
momsi <momsi@gaia>
2026-04-11 10:43:43 +0200

    simplify GPU config and use open=true

--- hosts/gaia/default.nix
+++ hosts/gaia/default.nix
@@ -26,30 +26,19 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.luks.reusePassphrases = true;
 
-  # NVIDIA GPU (system-specific)
-  hardware.enableRedistributableFirmware = true;
   hardware.graphics = {
     enable = true;
-    enable32Bit = true;
   };
+
+  services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
-    open = false;
+    open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  # Explicitly include and load nvidia modules at boot
-  boot.extraModulePackages = [ config.boot.kernelPackages.nvidiaPackages.latest ];
-  boot.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
-
-  # Blacklist nouveau to prevent conflicts with nvidia
-  boot.blacklistedKernelModules = [ "nouveau" ];
-
-  # Enable DRM modesetting for nvidia-drm (required for Wayland)
-  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
-
-  # Gaia-specific packages
+# Gaia-specific packages
   environment.systemPackages = with pkgs; [
     neofetch
     vscodium
